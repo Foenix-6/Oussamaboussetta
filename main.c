8ed5d7b22ff128352f0ce7601ea7682a16f8d17e
Oussamab6 <62991188+Oussamab6@users.noreply.github.com>
2020-06-11 16:38:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
+/**
+* @file main.c
+* @brief exécution de programme.
+* @author Oussama Boussetta
+* @version 0.1
+* @date june 07, 2020
+*
+* exécuter le programme pour l'entitée secondaire et l'ennemi .
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_ttf.h> 
+#include <time.h>
 #include "entity.h"
+#include "map.h"
 
 int main()
 {
-    int hauteur_fenetre = 1000,
+    int hauteur_fenetre = 1000,t,
         largeur_fenetre = 1500;
+	
+printf("donner type : \n 0 ->  bad || 1 -> good ");
+scanf("%d",&t);
 
    
 
@@ -35,24 +49,92 @@ int main()
 	background = IMG_Load("background.jpeg");
 	background_pos.x = 0;
 	background_pos.y = 0;
-
+// afficher background 
+	//SDL_BlitSurface(background,NULL,screen,&background_pos);
 
  
 //init entity
+
+input in;
 entity e;
-initialiser_entity (&e) ;
+map m;
+
+initialiser_entity (&e,t) ;
+
+personnage p;
+initialiser_perso (&p) ;
+SDL_EnableKeyRepeat(200, 0);
+SDL_Event event;
+initialiser_map (&m,screen,&e,&p) ;
+perso_map(&m,&p);
+entity_map(&m,&e);
+
+int a=1; 
+
 
 int done = 1;
         while (done)
-	{ 
-	update_entity(&e);
-	SDL_BlitSurface(background,NULL,screen,&background_pos);
-	afficher_entity(&e ,screen);	
-	SDL_Flip(screen);
-	}
-
+	{ 	 
+	e.col=0;
 	
+  	//in=getinput(&done,in);
+SDL_PollEvent(&event);
 
+           
+                        switch (event.type) 
+                        {
+
+                        case SDL_QUIT: 
+                                done = 0;
+                                break;
+
+                        //avec clavier
+                        case SDL_KEYDOWN:                     
+                                switch (event.key.keysym.sym) 
+                                {
+
+                                
+                                case SDLK_RIGHT: 
+                                         
+                                      p.perso_pos.x += 10;
+
+                                        break;
+
+                                case SDLK_LEFT: 
+                                        p.perso_pos.x -= 10;
+                                     
+                                         
+                                        break;
+                                case SDLK_UP: 
+                                        p.perso_pos.y -= 10;
+                                        break;
+
+                                case SDLK_DOWN: 
+                                      p.perso_pos.y += 10;
+                                        break;
+                                }
+                        } //fin switch
+
+
+
+	update_entity(&e,&p);
+	
+	SDL_BlitSurface(background,NULL,screen,&background_pos);
+	if(detect_collision(&p,&e)) a=gestion(&e);
+
+	if (a){ afficher_entity(&e ,screen);}
+    //if (e.col==0)
+	 afficher_perso(&p ,screen);
+	affiche_map(&m,screen,&e,&p);
+	mise_a_jour_map(&m ,screen,&p,&e);
+	SDL_Delay(10);
+
+	SDL_Flip(screen);
+
+	}
+	
+	
+	
 	SDL_FreeSurface(background);
 	SDL_Quit();
 
